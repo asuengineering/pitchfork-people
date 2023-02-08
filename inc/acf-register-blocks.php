b4cce049710a696a0b8408a8afd5de7ac19264c5
Steve Ryan <steve.ryan@asu.edu>
2023-02-08 11:45:59 -0700

    Moving profile blocks from PF Blocks to PF People

--- inc/acf-register-blocks.php
+++ inc/acf-register-blocks.php
@@ -13,6 +13,28 @@
  * Note: Blocks appear in the block picker IN THE ORDER THEY ARE LISTED HERE.
  * When adding a new block, please make sure to insert it an alphabetical order.
  */
+
 function pitchfork_people_acf_blocks_init() {
+
+	// Icons kept in a separate file.
+	require_once PITCHFORK_PEOPLE_BASE_PATH . '/acf-block-templates/icons.php';
+
+	// UDS Profiles, container block for directories.
+	register_block_type(
+		PITCHFORK_PEOPLE_BASE_PATH . 'acf-block-templates/profiles',
+		array(
+			'icon'     => $block_icon->users_rectangle,
+			'category' => 'pitchfork-blocks',
+		)
+	);
+
+	// UDS Profile (Person), manual data entry edition.
+	register_block_type(
+		PITCHFORK_PEOPLE_BASE_PATH . 'acf-block-templates/profile-manual',
+		array(
+			'icon'     => $block_icon->image_user,
+			'category' => 'pitchfork-blocks',
+		)
+	);
 }
 add_action( 'acf/init', 'pitchfork_people_acf_blocks_init' );
