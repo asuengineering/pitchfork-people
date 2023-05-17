5aa3de797d6fcb41c6b21f47bfd34d5896eabf64
Steve Ryan <steve.ryan@asu.edu>
2023-05-17 14:52:59 -0700

    Fix script enqueue naming conflict.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -6,19 +6,6 @@
  * @package pitchfork-people
  */
 
-// add_action('admin_enqueue_scripts', 'pitchfork_people_enqueue_admin_scripts');
-// function pitchfork_people_enqueue_admin_scripts() {
-
-// 	$the_plugin = get_plugin_data( PITCHFORK_PEOPLE_BASE_PATH . '/pitchfork-people.php' );
-// 	$plugin_version = $the_plugin['Version'];
-// 	$profile_js_version = $plugin_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/acf-update-profile.js' );
-// 	$profile_css_version = $plugin_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/admin.min.css' );
-
-// 	wp_enqueue_script( 'acf-update-profile', plugin_dir_url( __DIR__ ) . 'js/acf-update-profile.js',  array( 'acf-input' ), $profile_js_version , true );
-// 	wp_enqueue_style( 'acf-update-profile', plugin_dir_url( __DIR__ ) . 'css/admin.min.css',  array(), $profile_css_version );
-
-// }
-
 // Main styles/scripts enqueue. Front end of the website.
 add_action( 'wp_enqueue_scripts', 'pfpeople_enqueue_block_styles' );
 function pfpeople_enqueue_block_styles() {
@@ -30,9 +17,6 @@ function pfpeople_enqueue_block_styles() {
 	// Main plugin stylesheet.
 	wp_enqueue_style( 'pfpeople-block-styles', plugin_dir_url( __DIR__ ) . 'css/pfpeople.min.css', array( 'pitchfork-styles' ), $plugin_version );
 
-	// app-webdir-ui + init.
-	wp_enqueue_script ( 'app-webdir', plugin_dir_url( __DIR__ ) . 'src/app-webdir-ui/js/webdirUI.umd.js', array( 'wp-element', 'wp-components' ), null, false );
-	wp_enqueue_script ( 'app-webdir-init', plugin_dir_url( __DIR__ ) . 'js/app-webdir-init.js', array( 'wp-element', 'wp-components', 'app-webdir' ), null, false );
 }
 
 // Main styles/scripts enqueue. Block editor.
@@ -45,7 +29,7 @@ function pfpeople_enqueue_block_editor_styles() {
 	$block_styles_version     = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'css/pfpeople.min.css' );
 	$plugin_version 		  = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/block-variations.js' );
 
-	wp_enqueue_script( 'uds-block-variations', plugin_dir_url( __DIR__ ) . 'js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $plugin_version , true );
+	wp_enqueue_script( 'pfpeople-block-variations', plugin_dir_url( __DIR__ ) . 'js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $plugin_version , true );
 	wp_enqueue_style( 'pfpeople-block-styles', plugin_dir_url( __DIR__ ) . 'css/pfpeople.min.css', array(), $block_styles_version );
 	wp_enqueue_style( 'pfpeople-block-editor-styles', plugin_dir_url( __DIR__ ) . 'css/editor.min.css', array(), $block_editor_version );
 
