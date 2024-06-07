8fb49ec8977a39b1933f8232b037f512023d7d82
Steve Ryan <steve.ryan@asu.edu>
2024-06-07 16:17:04 -0700

    Uptick to v1.3.1

--- pitchfork-people.php
+++ pitchfork-people.php
@@ -7,7 +7,7 @@
  * Author URI:      https://engineering.asu.edu
  * Text Domain:     pitchfork_people
 <<<<<<< HEAD
- * Version:         1.3.0
+ * Version:         1.3.1
 =======
  * Version:         1.2
 >>>>>>> develop
