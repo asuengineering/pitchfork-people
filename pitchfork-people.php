7e1b3226369222eeb1db4edcb2f3038fd76b3b01
Steve Ryan <steve.ryan@asu.edu>
2024-06-24 15:05:12 -0700

    Uptick version to v1.4.0-beta.2

--- pitchfork-people.php
+++ pitchfork-people.php
@@ -6,7 +6,7 @@
  * Author:          Steve Ryan
  * Author URI:      https://engineering.asu.edu
  * Text Domain:     pitchfork_people
- * Version:         1.4.0-beta
+ * Version:         1.4.0-beta.2
  *
  * @package         pitchfork_people
  *
